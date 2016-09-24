ada5422405e70c60b9e4b47ca09a97ec818b0af7
Maurice Courtois <mc@courtoisconsulting.com>
2016-09-24 09:20:51 -0400

    - Cutting out all the fat away

--- golang/src/golang_sample/main.go
+++ golang/src/golang_sample/main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-	_ "expvar"
 	"flag"
-	"log"
-	"net/http"
-	_ "net/http/pprof"
-	"os"
-
-	"github.com/bakins/net-http-recover"
-	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/rpc"
 	"github.com/gorilla/rpc/json"
-	"github.com/justinas/alice"
-
 	"golang_sample/service"
+	"log"
+	"net/http"
 )
 
 // Let us set the listening host and port from
@@ -23,7 +15,6 @@ import (
 var addr = flag.String("addr", ":1234", "http service address") // Q=17, R=18
 
 func main() {
-	//initHttpRouter()
 	initRpcServer()
 }
 
@@ -35,21 +26,6 @@ func initRpcServer() {
 
 	s.RegisterService(new(service.Registration), "")
 
-	chain := alice.New(
-		func(h http.Handler) http.Handler {
-			return handlers.CombinedLoggingHandler(os.Stdout, h)
-		},
-		func(h http.Handler) http.Handler {
-			return handlers.ProxyHeaders(h)
-		},
-		handlers.CompressHandler,
-		func(h http.Handler) http.Handler {
-			return recovery.Handler(os.Stderr, h, true)
-		})
-
-	// httprof and expvar endpoints
-	//r.PathPrefix("/debug/").Handler(chain.Then(http.DefaultServeMux))
-
-	r.Handle("/rpc", chain.Then(s))
-	log.Println(http.ListenAndServe(":1234", r))
+	r.Handle("/rpc", s)
+	log.Println(http.ListenAndServe(*addr, r))
 }
